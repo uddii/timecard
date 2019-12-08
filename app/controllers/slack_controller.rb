422c6c2fb610b70707292f8a063dd2e229926b67
尾沼亮太 <onuma-ryota-jh@ynu.jp>
2019-12-08 15:46:36 +0900

    dateを追加

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,10 +1,12 @@
 class SlackController < ApplicationController
     protect_from_forgery :except => [:slackmessage]
-
+    require "date"
     def top
     
     end
     def slackmessage
+    
+       
         token = params[:token]
         if token != "XoeXsgC2PcytS5cuiazz7fSh"
             return
@@ -12,14 +14,16 @@ class SlackController < ApplicationController
         userName = params[:user_id]
         text1 = params[:text]
         if params[:trigger_word] == '出勤'
-        text = "<@#{userName}>\n出勤を確認しました！！"
+            now = Date.today 
+            text = "<@#{userName}>\n出勤を確認しました！！\n打刻時刻：#{now}"
         elsif params[:trigger_word] == '退勤'
-        text = "<@#{userName}>\n本日もお疲れ様 (^_^)"
+            now = Date.today 
+        text = "<@#{userName}>\n本日もお疲れ様 (^_^)\n打刻時刻：#{now}"
         else
             text = "<@#{userName}>\nもっとはたらけよ"
         end
         notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
         notifier.ping(text)
-   
+        
     end
 end
