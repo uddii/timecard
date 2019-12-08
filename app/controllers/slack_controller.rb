36413e83f97dc749e6355878df0b4aa336ebb547
尾沼亮太 <onuma-ryota-jh@ynu.jp>
2019-12-08 17:12:27 +0900

    @userName

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -14,14 +14,14 @@ class SlackController < ApplicationController
         if params[:trigger_word] == '出勤'
             nowTime = DateTime.now
             @start = Savetime.create(start: nowTime,who: @userName )
-            text = "<@#{userName}>\n出勤を確認しました！！\n出勤時刻：#{nowTime.hour}時#{nowTime.minute}分#{nowTime.second}秒"
+            text = "<@#{@userName}>\n出勤を確認しました！！\n出勤時刻：#{nowTime.hour}時#{nowTime.minute}分#{nowTime.second}秒"
         elsif params[:trigger_word] == '退勤'
             nowTime = DateTime.now
             @end = Savetime.create(end: nowTime,who: @userName)
-        text = "<@#{userName}>\n本日もお疲れ様 (^_^)\n退勤時刻：#{nowTime.hour}時#{nowTime.minute}分#{nowTime.second}秒"
+        text = "<@#{@userName}>\n本日もお疲れ様 (^_^)\n退勤時刻：#{nowTime.hour}時#{nowTime.minute}分#{nowTime.second}秒"
         redirect_to controller: 'slack', action: 'count', who: @userName
         else
-            text = "<@#{userName}>\nもっとはたらけよ"
+            text = "<@#{@userName}>\nもっとはたらけよ"
         end
         notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
         notifier.ping(text)
