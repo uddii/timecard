d1f9b9146df840c73d9722464b5d36b68f30d201
尾沼亮太 <onuma-ryota-jh@ynu.jp>
2019-12-08 17:06:44 +0900

    datetime

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -12,11 +12,11 @@ class SlackController < ApplicationController
         @userName = params[:user_id]
         text1 = params[:text]
         if params[:trigger_word] == '出勤'
-            nowTime = DateTime.today 
+            nowTime = DateTime.now
             @start = Savetime.create(start: nowTime,who: @userName )
             text = "<@#{userName}>\n出勤を確認しました！！\n出勤時刻：#{nowTime.hour}時#{nowTime.minute}分#{nowTime.second}秒"
         elsif params[:trigger_word] == '退勤'
-            nowTime = DateTime.today 
+            nowTime = DateTime.now
             @end = Savetime.create(end: nowTime,who: @userName)
         text = "<@#{userName}>\n本日もお疲れ様 (^_^)\n退勤時刻：#{nowTime.hour}時#{nowTime.minute}分#{nowTime.second}秒"
         redirect_to controller: 'slack', action: 'count', who: @userName
