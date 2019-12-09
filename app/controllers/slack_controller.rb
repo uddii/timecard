0a9ff1e59170cd1bd4f324275acdda846c5c1db0
尾沼亮太 <onuma-ryota-jh@ynu.jp>
2019-12-09 16:17:44 +0900

    time確認6

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -21,9 +21,9 @@ class SlackController < ApplicationController
             @day = Date.new
             @end = Savetime.create(end: nowTime,who: @userName,day: @day )
             @start  = Savetime.where(who: @userName).last(2).first
-        @text1 = @end.hour.to_i - @start.start.hour.to_i
-        @text2 = @end.min.to_i - @start.start.min.to_i
-        @text3 = @end.sec.to_i - @start.start.sec.to_i
+        @text1 = @end.end.hour.to_i - @start.start.hour.to_i
+        @text2 = @end.end.min.to_i - @start.start.min.to_i
+        @text3 = @end.end.sec.to_i - @start.start.sec.to_i
 
         text = "<@#{@userName}>\n本日もお疲れ様 (^_^)\n退勤時刻：#{@end.end.hour}時#{@end.end.min}分#{@end.end.sec}秒\n勤務時間：#{@text1}時間#{@text2}分#{@text3}秒"
 
