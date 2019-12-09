900214b8dcca7815200d93cfd8b74f01b3427e5a
尾沼亮太 <onuma-ryota-jh@ynu.jp>
2019-12-09 16:12:30 +0900

    time確認5

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -15,7 +15,7 @@ class SlackController < ApplicationController
             nowTime = Time.now
             @day = Date.new
             @start = Savetime.create(start: nowTime,who: @userName,day: @day )
-            text = "<@#{@userName}>\n出勤を確認しました！！\n出勤時刻：#{@start.hour}時#{@start.min}分#{@start.sec}秒"
+            text = "<@#{@userName}>\n出勤を確認しました！！\n出勤時刻：#{@start.start.hour}時#{@start.start.min}分#{@start.start.sec}秒"
         elsif params[:trigger_word] == '退勤'
             nowTime = Time.now
             @day = Date.new
@@ -25,7 +25,7 @@ class SlackController < ApplicationController
         @text2 = @end.min.to_i - @start.start.min.to_i
         @text3 = @end.sec.to_i - @start.start.sec.to_i
 
-        text = "<@#{@userName}>\n本日もお疲れ様 (^_^)\n退勤時刻：#{@end.hour}時#{@end.min}分#{@end.sec}秒\n勤務時間：#{@text1}時間#{@text2}分#{@text3}秒"
+        text = "<@#{@userName}>\n本日もお疲れ様 (^_^)\n退勤時刻：#{@end.end.hour}時#{@end.end.min}分#{@end.end.sec}秒\n勤務時間：#{@text1}時間#{@text2}分#{@text3}秒"
 
         else
             text = "<@#{@userName}>\nもっとはたらけよ"
