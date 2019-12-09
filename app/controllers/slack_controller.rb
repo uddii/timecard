b5ac3079bd1fa3a76a9a5d41daa27fface7be63c
尾沼亮太 <onuma-ryota-jh@ynu.jp>
2019-12-09 18:40:07 +0900

    ダブルチェック

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -11,15 +11,33 @@ class SlackController < ApplicationController
         end
         @userName = params[:user_id]
         text1 = params[:text]
+
+        if params[:trigger_word] =='ん、出勤してなくね？？'
+            return
+        end
+
+        if params[:trigger_word] =='ん、退勤してなくね？？'
+            return
+        end
+
         if params[:trigger_word] == '出勤'
+            @last  = Savetime.where(who: @userName).last(1).first
+            if @start.end.nil?
+                text = 'ん、退勤してなくね？？'
+                notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
+                notifier.ping(text)
+                return
+            else
             nowTime = Time.now
             @day = Date.new
             @start = Savetime.create(start: nowTime,who: @userName,day: @day )
             text = "<@#{@userName}>\n出勤を確認しました！！\n出勤時刻：#{@start.start.hour}時#{@start.start.min}分#{@start.start.sec}秒"
+            end
+
         elsif params[:trigger_word] == '退勤'
             @start  = Savetime.where(who: @userName).last(1).first
             if @start.start.nil?
-                text ='出勤してなくね？？'
+                text = 'ん、出勤してなくね？？'
                 notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
                 notifier.ping(text)
                 return
